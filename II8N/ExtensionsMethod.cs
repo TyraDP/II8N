f5070a521ca40373fba43d8aa7bec47813936abb
tyrad <tyrad@TDP>
2023-10-06 10:15:52 -0400

    Add project files.

---
+++ II8N/ExtensionsMethod.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace II8N
+{
+    internal static class ExtensionsMethod
+    {
+        //Rules for Extensions menthod
+        //1. Method and class have to be static
+        //2. use int
+        //3. Need to use the "this" keyword
+        //
+
+
+        public static int Add3(this int x)
+        {
+            return x + 3;
+        }
+
+        public static bool IsEven(this int y) 
+        {
+            //study the out parameter
+
+            return y % 2 == 0;
+               
+        }
+
+        public static bool IsOdd(this int y)
+        {
+            return !IsEven(y);
+        }
+    }
+}
---
+++ II8N/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace II8N
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string string1 = "ACT";
+            string string2 = "CAT";
+            //Trim to get rid of extra space
+            char[] string1_arr = string1.ToCharArray();
+
+
+
+            int numb = 3;
+            Console.WriteLine("Number is ");
+            Console.WriteLine(numb.IsEven() ? "even" : "odd");
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("fr-CA");
+
+
+
+            Console.WriteLine(Properties.strings.Prompt);
+            string name = Console.ReadLine();
+            Console.WriteLine(Properties.strings.Greeting + name);
+
+            int x = 2;
+
+            x = x.Add3();
+
+            Console.WriteLine(x);
+
+
+        }
+
+        //sort the charcater array
+
+    }
+}
